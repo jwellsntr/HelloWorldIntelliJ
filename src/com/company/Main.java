6b4d5237148ec84e72351cd558db886b8f55552a
John Wells <42276167+jwellsntr@users.noreply.github.com>
2019-08-18 15:54:21 -0400

    Increased loop to 50 in Looper Branch
    In Looper branch, i increased the loop to 50. But will have to merge.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        for(int i=0;i<=40;i++) {
+        for(int i=0;i<=50;i++) {
             System.out.println("Hello World - Jen was here!");
         }
     }
