32c6e74e84195d0fbb3cf1931b9d49eb0544dbcc
jwellsntr <42276167+jwellsntr@users.noreply.github.com>
2019-08-18 15:48:46 -0400

    Added local class incrementNumber. Also added a new separate class incrementNumber, but haven't called it yet.

---
+++ src/incrementNumber.java
@@ -0,0 +1,7 @@
+public class incrementNumber{
+    public static int main(int number) {
+        System.out.println("the number in: " + number);
+        //number = number + 12;
+    return(number);
+    }
+}
